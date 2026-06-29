0547e0d955114927eea4527a47272dbdd5e44526
Navaneetha M <navaneetham790@gmail.com>
2026-06-29 10:01:16 +0530

    Refactor calculateInterest method in RDAccount

--- src/com/wipro/bank/acc/RDAccount.java
+++ src/com/wipro/bank/acc/RDAccount.java
@@ -10,11 +10,10 @@ public class RDAccount extends Account{
 		return principal*tenure*12;
 	}
 	@Override
-    public float calculateInterest() {
-        int n = 4; 
-        float r = rateOfInterest / 100; 
-        int t = tenure;
-        float amount = (float) (principal * (Math.pow((1 + r / n), n * t)));
-        return amount - principal;
+	public float calculateInterest() {
+	    int months = tenure * 12;
+	    float interest = (principal * months * (months + 1) * rateOfInterest)
+	            / (2 * 12 * 100);
+	    return interest;
 	}
 }
